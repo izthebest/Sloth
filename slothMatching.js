e5aa39efb276ec5b049c3d5fe9b840fd5c4c3971
garima35 <garthapa@gmail.com>
2016-08-19 10:52:20 -0700

    matching system js

---
+++ slothMatching.js
@@ -0,0 +1,76 @@
+
+var userCollection = 
+var currentUser = {username, };
+var sameZipList = ;
+var users = ;
+var zipcode = ;
+var matchList = [];
+
+for (var users = 0; users < userCollection.length; users += 1) {
+    if (zipcode == currentuser.zipcode){
+        add user to sameZipList []
+    }
+}
+for (var users = 0; users < sameZipList.length; users += 1) {
+    if (attributes == currentUser.attributes){
+        add user to matchList
+    }
+    show matchList []
+    
+}
+
+if (currentUser anx = "on"){
+    for (var anxusers = 0; anxusers < sameZipList.length; anxusers += 1) {
+        if (sameZipList[anxusers].anx == "on"){
+            add user to matchList //append sameZipList[anxusers] to matchList
+        }
+    }
+}
+
+if (currentUser str = "on"){
+    for (var strusers = 0; strusers < sameZipList.length; strusers += 1) {
+        if (sameZipList[strusers].str == "on"){
+            add user to matchList //append sameZipList[strusers] to matchList
+        }
+    }
+}
+
+if (currentUser rel = "on"){
+    for (var relusers = 0; relusers < sameZipList.length; relusers += 1) {
+        if (sameZipList[relusers].rel == "on"){
+            add user to matchList //append sameZipList[relusers] to matchList
+        }
+    }
+}
+
+if (currentUser bul = "on"){
+    for (var bulusers = 0; bulusers < sameZipList.length; bulusers += 1) {
+        if (sameZipList[bulusers].bul == "on"){
+            add user to matchList //append sameZipList[bulusers] to matchList
+        }
+    }
+}
+
+if (currentUser dep = "on"){
+    for (var depusers = 0; depusers < sameZipList.length; depusers += 1) {
+        if (sameZipList[depusers].dep == "on"){
+            add user to matchList //append sameZipList[depusers] to matchList
+        }
+    }
+}
+
+if (currentUser low = "on"){
+    for (var lowusers = 0; lowusers < sameZipList.length; lowusers += 1) {
+        if (sameZipList[lowusers].low == "on"){
+            add user to matchList //append sameZipList[lowusers] to matchList
+        }
+    }
+}
+
+if (currentUser sui = "on"){
+    for (var suiusers = 0; suiusers < sameZipList.length; suiusers += 1) {
+        if (sameZipList[suiusers].sui == "on"){
+            add user to matchList //append sameZipList[suiusers] to matchList
+        }
+    }
+}
